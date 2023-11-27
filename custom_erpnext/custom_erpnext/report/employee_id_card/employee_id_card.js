7b74955fa60259117f4fb177e55827a9310d94e5
RafeedMohammad <rasheedtahmeed@gmail.com>
2023-11-27 16:42:51 +0600

    add id card report and change table-head size to 7 from 7.5

---
+++ custom_erpnext/custom_erpnext/report/employee_id_card/employee_id_card.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2023, Lithe-Tech Limited and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employee ID Card"] = {
+	"filters": [
+        
+		{
+			"fieldname": "company",
+			"fieldtype": "Link",
+			"label": "Company",
+			"mandatory": 0,
+			"default": frappe.defaults.get_user_default("Company"),
+			"options": "Company",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "employee",
+			"fieldtype": "Link",
+			"label": "Employee",
+			"mandatory": 0,
+			// "default":"Tah 120",
+			"options": "Employee",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "department",
+			"fieldtype": "Link",
+			"label": "Department",
+			"mandatory": 0,
+			// "default":"Admin (GSD) - QSTML",
+			"options": "Department",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "designation",
+			"fieldtype": "Link",
+			"label": "Designation",
+			"mandatory": 0,
+			"options": "Designation",
+			"wildcard_filter": 0
+		},
+		{
+			"fieldname": "status",
+			"fieldtype": "Data",
+			"label": "Employee Status",
+			"mandatory": 0,
+			"default":"Active",
+			"options": "Active,Left",
+			"wildcard_filter": 0
+		},
+			
+	]
+};
+
---
+++ custom_erpnext/custom_erpnext/report/employee_id_card/employee_id_card.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+import frappe
+from frappe import _
+
+
+def execute(filters= None):
+	if not filters:
+		filters = {}
+	columns = get_columns()
+	data = get_data(filters)
+	#index_of_status = columns.index("Status:Data/:120")
+	#report_summary = get_report_summary(data,index_of_status)
+	return columns, data
+	
+
+
+def get_columns():
+	return [
+		_("Name") + ":Data/Employee:120",
+		_("Company") + ":Data/Employee:120",
+		_("Image") + ":Data/Employee:120",
+		_("Employee Name") + ":Data/Employee:120",
+		_("Department") + ":Data/Employee:120",
+		_("Designation") + ":Data/Employee:120",
+		_("Joining Date") + ":Data/Employee:120",
+		_("Blood group") + ":Data/Employee:120",
+		_("Phone No") + ":Data/Employee:120",
+		_("Company logo") + ":Data:120",
+	]
+
+def get_data(filters):
+	conditions, filters = get_conditions(filters)
+	result = frappe.db.sql("""SELECT emp.name,emp.company,emp.image , emp.employee_name, emp.department ,emp.designation, emp.date_of_joining,emp.blood_group, emp.cell_number ,com.company_logo FROM tabEmployee as emp INNER JOIN tabCompany as com ON emp.company = com.name  where %s""" 
+	% conditions, as_list=1)
+
+	for i in range(0,len(result)):
+		if result[i][2] is None:
+			result[1][2]=result[i][-1]
+
+	# data = frappe.get_all(
+	# 	"Employee",
+	# 	filters=conditions,
+	# 	fields=["name", "employee_name", "default_shift"],
+	# )
+
+	return result
+	
+
+def get_conditions(filters):
+    conditions="" 
+    if filters.get("company"): conditions += "company= '%s'" % filters["company"]
+    if filters.get("employee"): conditions += " and employee= '%s'" % filters["employee"]
+    if filters.get("department"): conditions += "and department= '%s'" % filters["department"]
+    if filters.get("designation"): conditions += " and designation='%s'" % filters["designation"]
+    if filters.get("status"): conditions += " and status='%s'" % filters["status"]
+	
+    return conditions, filters
+
